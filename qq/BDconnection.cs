fc92beb21b68dcf8ab4f180860b09bb43b90fc8e
227845 <227845@KB37-316-C10>
2025-04-28 14:13:43 +0300

    Добавьте файлы проекта.

---
+++ qq/BDconnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qq
+{
+    internal class BDconnection
+    {
+        private static k8sLoverEntities _k8SLoverEntities;
+
+        public static k8sLoverEntities DB = GetContext();
+        public static k8sLoverEntities GetContext()
+        {
+            if(_k8SLoverEntities == null )
+            {
+                _k8SLoverEntities = new k8sLoverEntities();
+            }
+            return _k8SLoverEntities;
+        }
+    }
+}
---
+++ qq/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace qq
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           // AddItems();
+            LoadCompanies();
+        }
+
+        public void AddItems()
+        {
+            Companies qq = new Companies();
+            qq.industry = "test";
+            qq.name = "test";
+            BDconnection.DB.Companies.Add(qq);
+            //BDconnection.
+            BDconnection.DB.SaveChanges();
+
+            
+        }
+
+        private void LoadCompanies()
+        {
+            CompaniesGrid.ItemsSource = BDconnection.DB.Companies.ToList();
+        }
+    }
+}
